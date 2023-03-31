45891dcc5acab8138e7a51e7f88458762dc412c5
AlanMartinezDev <alanmartiola@gmail.com>
2023-03-31 10:22:41 +0200

    usuarios, preguntas, bloqueos

--- index.js
+++ index.js
@@ -1,35 +1,90 @@
 const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-const httpServer = createServer(app);
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+const fs = require("fs");
 
 app.use(express.static("public"));
 
-const io = new Server(httpServer, {});
+let preguntas = [];
 
-
-io.on("connection", (socket) => {
-  
-    console.log('Connectat un client...')
-
-    socket.on("nickname", function(data) {
-            console.log(data.nickname)
-            
-            // respondre al que ha enviat
-            socket.emit("nickname rebut",{"response":"ok"})
-
-            // respondre a la resta de clients menys el que ha enviat
-            // socket.broadcast.emit(/* ... */);
-
-            // Totes les funcions disponibles les tenim a
-            //  https://socket.io/docs/v4/emit-cheatsheet/
-    })
-
+fs.readFile("preguntas.json", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  preguntas = JSON.parse(data);
+  console.log("Preguntas cargadas:", preguntas);
 });
 
-httpServer.listen(3000, ()=>
-    console.log(`Server listening at http://localhost:3000`)
-);
+let players = [];
 
+io.on("connection", (socket) => {
+  console.log("Un cliente se ha conectado.");
+
+  socket.on("join", ({ nickname }, callback) => {
+    console.log(`El jugador ${nickname} se ha unido.`);
+
+    if (players.some((player) => player.name === nickname)) {
+      callback({ status: "error", message: "El nombre de usuario ya está en uso." });
+    } else {
+      players.push({ id: socket.id, name: nickname, score: 0 });
+      io.emit("players", players);
+      if (players.length === 1) {
+        startGame();
+      }
+      callback({ status: "success" });
+    }
+  });
+
+  socket.on("answer", (data) => {
+    console.log(`El jugador ${data.nickname} ha respondido: ${data.answer}`);
+    let player = players.find((p) => p.name === data.nickname);
+    if (player) {
+      let question = preguntas[data.question];
+      let answer = question.answers[data.answer];
+      if (answer.correct) {
+        player.score += question.time;
+        io.emit("players", players); // Emit the players event to update the score
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`El jugador ${socket.id} se ha desconectado.`);
+    players = players.filter((p) => p.id !== socket.id);
+    io.emit("players", players);
+  });
+
+  socket.on("restart", () => {
+    if (players.length > 0) {
+      startGame();
+    }
+  });
+});
+
+function startGame() {
+  let questionIndex = 0;
+  let intervalId = setInterval(() => {
+    if (questionIndex >= preguntas.length) {
+      clearInterval(intervalId);
+      endGame();
+      return;
+    }
+    let question = preguntas[questionIndex];
+    io.emit("question", { index: questionIndex, text: question.text, answers: question.answers, time: question.time });
+    questionIndex++;
+  }, 11000);
+}
+
+function endGame() {
+  console.log("El juego ha terminado");
+  players.sort((a, b) => b.score - a.score);
+  let winners = players.filter((p) => p.score === players[0].score);
+  let winnerNames = winners.map((w) => w.name);
+  io.emit("winners", winnerNames);
+}
+
+httpServer.listen(3000, () => {
+  console.log("Servidor escuchando en http://localhost:3000");
+});
--- public/main.js
+++ public/main.js
@@ -1,17 +1,79 @@
-
 const socket = io();
 
-const nicknameInput = document.getElementById("nicknameInput");
 const sendButton = document.getElementById("sendButton");
-sendButton.addEventListener("click", send)
+const nicknameInput = document.getElementById("nicknameInput");
+const game = document.getElementById("game");
+const questionElement = document.getElementById("question");
+const answersElement = document.getElementById("answers");
+const restartButton = document.getElementById("restartButton");
 
-function send() {
-    socket.emit("nickname", {nickname: nicknameInput.value} )
+sendButton.addEventListener("click", () => {
+  const nickname = nicknameInput.value;
+  if (!nickname) {
+    return;
+  }
+
+  socket.emit("join", { nickname }, (response) => {
+    if (response.status === "error") {
+      alert(response.message);
+    } else {
+      sendButton.disabled = true;
+      nicknameInput.disabled = true;
+      game.style.display = "block";
+    }
+  });
+});
+
+function displayQuestion(question) {
+  questionElement.textContent = question.text;
+  answersElement.innerHTML = "";
+
+  question.answers.forEach((answer, index) => {
+    const button = document.createElement("button");
+    button.textContent = answer.text;
+    button.onclick = () => {
+      socket.emit("answer", { nickname: nicknameInput.value, question: question.index, answer: index });
+      disableAnswerButtons();
+    };
+    answersElement.appendChild(button);
+  });
 }
 
-socket.on('nickname rebut', function(data) {
+socket.on("question", (data) => {
+  displayQuestion(data);
+  enableAnswerButtons();
+});
 
-    console.log(data)
+const pointsElement = document.getElementById("points");
 
-})
+socket.on("players", (players) => {
+  const player = players.find((p) => p.name === nicknameInput.value);
+  if (player) {
+    pointsElement.textContent = player.score;
+  }
+});
 
+socket.on("winners", (winners) => {
+  questionElement.textContent = "El juego ha terminado";
+  answersElement.innerHTML = "Ganadores: " + winners.join(", ");
+  restartButton.style.display = "block";
+});
+
+restartButton.addEventListener("click", () => {
+  socket.emit("restart");
+  restartButton.style.display = "none";
+});
+
+function disableAnswerButtons() {
+  const buttons = answersElement.getElementsByTagName("button");
+  for (const button of buttons) {
+    button.disabled = true;
+  }
+}
+
+function enableAnswerButtons() {
+  const buttons = answersElement.getElementsByTagName("button");
+  for (const button of buttons) {
+    button.disabled = false;
+  }
+}
